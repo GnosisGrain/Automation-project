5e4898fc37165b1a007cc328bbf55c3831872f57
GnosisGrain <103087417+GnosisGrain@users.noreply.github.com>
2022-12-06 03:18:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,9 @@
 from Server import Server
 
 def print_program_info():
-    # TODO - Change your name
     print("Server Automator v0.1 by Doc McDowell")
 
-# This is the entry point to our program
+
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
